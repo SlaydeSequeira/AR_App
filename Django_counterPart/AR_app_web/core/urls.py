6fae0e741d98696991691b4f55c23a32bce2366f
Chhand Chaughule <92355919+chhand2808@users.noreply.github.com>
2024-10-15 22:40:43 +0530

    Additional pages are added as requested (#4)

--- Django_counterPart/AR_app_web/core/urls.py
+++ Django_counterPart/AR_app_web/core/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('services/', views.services, name='services'),
     path('blog/', views.blog, name='blog'),
     path('faq/', views.faq, name='faq'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('team/', views.team, name='team'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('testimonials/', views.testimonials, name='testimonials'),
+    path('terms-of-service/', views.terms_of_service, name='terms_of_service'),
 ]
