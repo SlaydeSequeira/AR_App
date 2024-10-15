d83bdf13ac1cdab082fad4096ef26547864c3238
chhand2808 <chhandchaughule123@gmail.com>
2024-10-15 21:58:50 +0530

    Added multi page support

--- Django_counterPart/AR_app_web/core/urls.py
+++ Django_counterPart/AR_app_web/core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('blog/', views.blog, name='blog'),
+    path('faq/', views.faq, name='faq'),
 ]
